dd0eeab0b99168534be298cbfb9ce09ae2374608
rajvichauhan77 <chauhanrajvi77@gmail.com>
2025-06-23 17:19:24 +0530

    sfd

--- index.js
+++ index.js
@@ -12,16 +12,21 @@ app.use(express.urlencoded())
 
 app.set("view engine", "ejs")
 
+app.get()
 
-app.get("/delete", (req,res) => {
-    let userId = req.query.id
+
+app.get("/delete/:id", (req,res) => {
+    let userId = req.params.id
     data = data.filter((ele) => ele.id != userId)
     res.redirect('/')
+    // console.log(req.params.id)
 })
 
 app.post('/insert', (req,res) => {
     data.push(req.body)
     res.redirect('/')
+    // console.log(req.body)
+
 })
 
 app.get('/', (req,res) => {
